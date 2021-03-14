4b74dbe6fe76d25cc55db9979091a211adf2b52e
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-03-14 16:54:48 +0530

    Added a FixedUpdate Loop

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+#include <chrono>
+
+using namespace std::chrono;
+
 #include "raylib.h"
+
+typedef steady_clock::time_point TimePoint;
+typedef duration <double> Duration;
+
+#define FPS 60
+
+const float dt = 1 / FPS;
 
 int main(void)
 {
@@ -8,8 +19,21 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Apple 2D");
     SetTargetFPS(60);
     
+    TimePoint frameStart = steady_clock::now();
+    double accumulator = 0;
+    
     while(!WindowShouldClose())
     {
+        const TimePoint currentTime = steady_clock::now();
+        duration<double> time_span = duration_cast<duration<double>>(currentTime - frameStart);
+        
+        accumulator += time_span.count();
+        
+        while (accumulator > dt)
+        {
+            accumulator -= dt;
+        }
+        
         BeginDrawing();
         ClearBackground(RAYWHITE);
         EndDrawing();
