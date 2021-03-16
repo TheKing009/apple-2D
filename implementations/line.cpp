1c9ddd55c2fe394669d27a154faf273b6416ae03
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-03-16 15:22:59 +0530

    Removed line vs point check

--- implementations/line.cpp
+++ implementations/line.cpp
@@ -8,21 +8,17 @@ Line::Line()
     this->m = this->c = 0.0;
 }
 
+Line::Line(const double &m, const double &c)
+{
+    this->m = m;
+    this->c = c;
+}
+
 Line::Line(const Vector2 &a, const Vector2 &b)
 {
     double dy = a.y - b.y;
     double dx = a.x - b.x;
 
     this->m = dy/dx;
-    this->c = 0.0;
-}
-
-bool Line::PointOnLine(const Vector2& point)
-{
-    double value = this->m * point.x + this->c - point.y;
-    if (value < __FLT_EPSILON__)
-    {
-        return true;
-    }
-    return false;
+    this->c = b.y - (this->m * b.x);
 }
