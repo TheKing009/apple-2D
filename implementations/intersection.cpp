f677af8424a9d62214661b6cbf390b8b6cbe0da6
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-03-21 12:16:24 +0530

    Added an intersection class that contains logic for checking primitive intersections

--- implementations/intersection.cpp
+++ implementations/intersection.cpp
@@ -2,7 +2,7 @@
 
 using namespace apple2D;
 
-bool Intersection::LineVsPoint( Line &line,  Vector2 &point)
+bool Intersection::LineVsPoint( Line line,  Vector2 point)
 {
     float slope = line.GetSlope();
     float intercept = line.GetIntercept();
@@ -12,7 +12,7 @@ bool Intersection::LineVsPoint( Line &line,  Vector2 &point)
     return (value == 0.0);
 }
 
-bool Intersection::CircleVsPoint ( Circle& circle,  Vector2 &point)
+bool Intersection::CircleVsPoint ( Circle circle,  Vector2 point)
 {
     float radius = circle.GetRadius();
     Vector2 center = circle.GetCenter();
@@ -21,7 +21,7 @@ bool Intersection::CircleVsPoint ( Circle& circle,  Vector2 &point)
     return value <= (radius * radius);
 }
 
-bool Intersection::AABBvsPoint ( AABB& aabb,  Vector2 &point)
+bool Intersection::AABBvsPoint ( AABB aabb,  Vector2 point)
 {
     Vector2 max = aabb.GetMax();
     Vector2 min = aabb.GetMin();
@@ -33,3 +33,53 @@ bool Intersection::AABBvsPoint ( AABB& aabb,  Vector2 &point)
         point.y <= max.y
     );
 }
+
+bool Intersection::SegmentVsPoint(Segment seg, Vector2 point)
+{
+    Vector2 a = seg.GetEnd() - seg.GetStart();
+    Vector2 b = point - seg.GetStart();
+
+    float cross = Vector2::Cross(a, b);
+    bool distanceCheck = a.LengthSqr() > b.LengthSqr();
+
+    return (cross == 0) && distanceCheck;
+}
+
+bool Intersection::RectVsPoint (Box rect, Vector2 point)
+{
+    Vertices verts = rect.GetVertices();
+    float rotation = rect.GetRotation();
+
+    Vector2 rotatedPoint = point.Rotate (rotation);
+
+    // Implement this
+    return false;
+}
+
+/* Two lines intersect if they are not parallel */
+
+bool Intersection::LineVsLine(Line line1, Line line2)
+{
+    return !(abs(line1.GetSlope()) == abs(line2.GetSlope()));
+}
+
+/* Consider a line with the equation y = mx + c;
+If it is a tangent to the circle with center (h, K) and radius r;
+then, the perpendicular distance of the center from the line = r;
+therefore mh - k + c / (1 + m*m) <= r; for intersection; */
+
+bool Intersection::LineVsCircle(Line line, Circle circle)
+{
+    Vector2 center = circle.GetCenter ();
+    float radius = circle.GetRadius ();
+
+    float m = line.GetSlope();
+    float c = line.GetIntercept();
+
+    float denominator = 1 + m * m;
+    float numerator = m * center.x - center.y + c;
+    numerator *= numerator;
+
+    return numerator / denominator <= circle.GetRadius();
+}
+
--- includes/intersection.h
+++ includes/intersection.h
@@ -11,9 +11,18 @@ namespace apple2D
         private:
             Intersection(); // No instancing allowed
         public:
-            static bool LineVsPoint ( Line&,  Vector2&);
-            static bool CircleVsPoint ( Circle&,  Vector2&);
-            static bool AABBvsPoint ( AABB&,  Vector2&);
+            // Point Overlap checks
+            static bool LineVsPoint ( Line,  Vector2);
+            static bool CircleVsPoint ( Circle,  Vector2);
+            static bool AABBvsPoint ( AABB,  Vector2);
+            static bool RectVsPoint (Box, Vector2);
+            static bool SegmentVsPoint (Segment, Vector2);
+
+            // line overlap checks.
+            static bool LineVsLine(Line, Line);
+            static bool LineVsCircle(Line, Circle);
+            static bool LineVsRect(Line, Box);
+            static bool LinevsAABB(Line, AABB);
     };
 }
 
